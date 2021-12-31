2a0daebe8e55a825cdb3f330c8de32753a50d4ae
Musab1258 <67251181+Musab1258@users.noreply.github.com>
2021-12-31 23:56:00 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
-function me () {
-  return 'me'
-}
+$('#mobile-toggle').on('click', function() {
+    $('#menu').addClass('show')
+})
+
+$('#close-toggle').on('click', function() {
+    $('#menu').removeClass('show')
+})
